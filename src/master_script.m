2173d48d0b72feeea17abe14cff375a7c258d272
jmohl <mohlymohly@gmail.com>
2019-05-13 18:08:24 -0400

    running on final yoko data
    updated with option to run only a single subject.

--- src/master_script.m
+++ src/master_script.m
@@ -14,17 +14,21 @@
 
 % hardcoded parameters
 local_directory = 'C:\Users\jtm47\Documents\Projects\tidy_data_AVD\';
-all_ver = {1,2,'HU'}; %1,2, or 'HU' for avd1, avd2, and human paradigm versions respectively
-%all_ver = {2};
+%all_ver = {1,2,'HU'}; %1,2, or 'HU' for avd1, avd2, and human paradigm versions respectively
+all_ver = {2}; %only running on version 2 to update
+limited_subjects = 1;subjects = {'Yoko'};  %option to run only specific subjects
+
 %adding paths
 cd(local_directory)
-addpath('src','results','data')
+addpath('src','results','data','src\beethoven_gread')
 
 for ver = 1:length(all_ver)
     avd_ver = all_ver{ver};
     %load metadata containing file IDs
     metadata = load_metadata(avd_ver);
-    
+    if limited_subjects
+        metadata = metadata(contains(metadata.beet_name,subjects),:);
+    end
     %get parameters related to this paradigm version
     paradigm_params = get_paradigm_params(avd_ver);
     
